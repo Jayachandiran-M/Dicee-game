9654924c169c2f1e5aba598114528a233c366303
Jayachandiran-M <jayachandiran666@gmail.com>
2025-12-29 10:52:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomImage="images/"+"dice"+randomNumber1+".png";
+var image1=document.querySelector(".img1");
+image1.setAttribute("src",randomImage);
+var randomNumber1=Math.floor(Math.random()*6)+1;
+var randomImage="images/"+"dice"+randomNumber1+".png";
+var image2=document.querySelector(".img2");
+image2.setAttribute("src",randomImage);
